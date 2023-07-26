de26df759acce2ea833c6a0fef079ea14c0c880e
liansen <1979118406@qq.com>
2023-07-26 16:23:19 +0800

    第一次提交

---
+++ src/main/java/com/atguigu/sunrise/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.sunrise;
+
+/**
+ * @author liansen
+ * @create 07-26-16:21
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
